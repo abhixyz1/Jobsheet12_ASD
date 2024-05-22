2c3c5053b96b93916b7b3c39c93120c2771d555a
abhixyz1 <abhinayazrf@gmail.com>
2024-05-23 05:06:44 +0700

    Praktikum Double Linked List

---
+++ doublelinkedlists/Node.java
@@ -0,0 +1,17 @@
+package doublelinkedlists;
+
+/**
+ * @author Abhinaya
+ */
+
+public class Node {
+    int data;
+    Node prev;
+    Node next;
+    
+    public Node(Node prev, int data, Node next) {
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+    }
+}
