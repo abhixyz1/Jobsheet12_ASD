a53526f06c75b6addeeb516bcec6a5c40e017ac3
abhixyz1 <abhinayazrf@gmail.com>
2024-05-23 20:37:28 +0700

    Praktikum Double Linked List

--- doublelinkedlists/DoubleLinkedListMain.java
+++ doublelinkedlists/DoubleLinkedListMain.java
@@ -7,23 +7,21 @@ package doublelinkedlists;
 public class DoubleLinkedListMain {
     public static void main(String[] args) throws Exception {
         DoubleLinkedLists dll = new DoubleLinkedLists();
-        dll.addLast(50);
-        dll.addLast(40);
-        dll.addLast(10);
-        dll.addLast(20);
         dll.print();
         System.out.println("Size : "+ dll.size);
         System.out.println("======================================");
-        dll.removeFirst();
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
         dll.print();
         System.out.println("Size : "+ dll.size);
         System.out.println("======================================");
-        dll.removeLast();
+        dll.add(40, 1);
         dll.print();
         System.out.println("Size : "+ dll.size);
         System.out.println("======================================");
-        dll.remove(1);
-        dll.print();
-        System.out.println("Size : "+ dll.size);
+        System.out.println("Data awal pada Linked Lists adalah : "+ dll.getFirst());
+        System.out.println("Data akhir pada Linked Lists adalah : "+ dll.getLast());
+        System.out.println("Data indeks ke-1 pada Linked Lists adalah : "+ dll.get(1));
     }
 }
